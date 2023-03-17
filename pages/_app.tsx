31d3f78366e09f438db925e6d8c5d5d4d1fc1fbb
klm hyeon woo <gentlemonster77@naver.com>
2023-03-17 23:24:32 +0900

    Revert "[Feature] 랜딩 페이지 제작 (#1)"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,6 @@
-import GlobalStyles from "@/styles/GlobalStyles";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import Head from "next/head";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/font-applesdgothicneo@1.0/all.min.css"
-        />
-        <title> 멋쟁이사자처럼 </title>
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />
 }
-//gud dlrj gksrmffh djzp qkRnsms rjdpdy alcls aorqnr gkskeh ahfmrpTsp bb
-
-//형 이거 안글러 어케 바꾸는 거에요 밈친 맥북 마나도 모르게ㅆ네 ㅠㅠ
