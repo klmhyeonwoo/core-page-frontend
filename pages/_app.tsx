0906323b2d60c8f9e04f4eed05a34899cc676a49
klmhyeonwooo <gentlemonster77@naver.com>
2023-03-18 00:34:09 +0900

    Feature : 랜딩 페이지 제작 (#1)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,33 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0"
+        />
+        <meta name="theme-color" content="#000000" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://knu-likelion.org/" />
+        <meta property="og:title" content="멋쟁이사자처럼 강남대학교" />
+        <meta property="og:image" content="https://url.kr/mzrxb6" />
+        <meta
+          property="og:description"
+          content="멋쟁이사자처럼 강남대학교 지원 사이트"
+        />
+        <meta
+          property="og:site_name"
+          content="멋쟁이사자처럼 강남대학교 지원 사이트"
+        />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta
+          name="description"
+          content="멋쟁이사자처럼 강남대학교 지원 사이트"
+        />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/font-applesdgothicneo@1.0/all.min.css"
